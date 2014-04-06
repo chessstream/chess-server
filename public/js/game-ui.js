b4a39873ec9e56c56278a0722627cc6a8e4ab141
Daylen Yang <services@daylenyang.com>
2014-04-06 07:10:41 -0700

    Try to communicate with Stockfish

--- public/js/game-ui.js
+++ public/js/game-ui.js
@@ -2,7 +2,7 @@ var socket = io.connect();
 socket.emit('init', {url: document.URL});
 
 $(function(){
-  var start = game.fen; // fen is set in game view
+  var start = fen; // fen is set in game view
   var board = new ChessBoard('board', {
     pieceTheme: '/img/chesspieces/{piece}.svg',
     position: start
@@ -13,10 +13,8 @@ $(function(){
    */
   function updateBoard(game){
     board.position(game.fen);
-    $('.analysis-text').html('');
-    $('.analysis-text').append('<p>' + (game.sideToMove === 0) ? 'White' : 'Black' + ' to move</p>');
+    // TODO update the stats
   }
 
   socket.on('update', updateBoard);
-  updateBoard(game);
 });
