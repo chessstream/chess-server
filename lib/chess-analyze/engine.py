affd8aa5ffb364a9c6937249c90aaebac58f6197
Daylen Yang <services@daylenyang.com>
2014-04-06 08:14:20 -0700

    Update game information from JSON

--- lib/chess-analyze/engine.py
+++ lib/chess-analyze/engine.py
@@ -3,15 +3,6 @@ import subprocess
 stockfish = subprocess.Popen(["./../../Stockfish/src/stockfish"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 
 def get_best_move(fen, move_time):
-	# moves_as_str = ' '.join(moves_list)
-	# stockfish.stdin.write(('setoption name Threads value 4\n').encode('utf-8'))
-	# stockfish.stdin.flush()
-	# stockfish.stdin.write(('setoption name Hash value 512\n').encode('utf-8'))
-	# stockfish.stdin.flush()
-	# stockfish.stdin.write(('setoption name OwnBook value true\n').encode('utf-8'))
-	# stockfish.stdin.flush()
-	# stockfish.stdin.write(('setoption name Skill Level value ' + skill_level + '\n').encode('utf-8'))
-	# stockfish.stdin.flush()
 	stockfish.stdin.write(('position fen ' + fen + '\n').encode('utf-8'))
 	stockfish.stdin.flush()
 	stockfish.stdin.write(('go movetime ' + move_time + '\n').encode('utf-8'))
@@ -34,5 +25,5 @@ def get_best_move(fen, move_time):
 			mate = False
 			break
 	
-	print "{ bestmove: " + str(bestmove.split()[1]) + ", score: " + analysis + ", isMate: " + ("True" if mate else "False") + " }"
+	print "{ bestmove: " + str(bestmove.split()[1]) + ", score: " + analysis + ", isMate: " + ("true" if mate else "false") + " }"
 
