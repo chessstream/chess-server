51f00b91d5aed8b6bb42c674e63488101999d741
Davis Foote <djfoote@berkeley.edu>
2014-04-06 07:43:08 -0700

    implemented board string rotation

--- lib/chess-cv/py-chess-cv/chess_cv/string_utils.py
+++ lib/chess-cv/py-chess-cv/chess_cv/string_utils.py
@@ -25,27 +25,60 @@ def compare_square_array_to_location_array(squares, locations):
 				disappeared = (row, col)
 			elif squares[row][col].has_piece and not locations[row][col].piece:
 				appeared = (row, col)
+			elif squares[row][col].has_piece and locations[row][col].piece \
+					and squares[row][col].piece_color != \
+						locations[row][col].color:
+				taken = (row, col)
 
-	return disappeared, appeared
+	return disappeared, appeared, taken
 
-def update_location_array(locations, squares, disappeared, appeared):
+def update_location_array(locations, squares, disappeared, appeared, taken):
 	"""Destructive: changes locations in place.
-	appeared and disappeared are (row, col), not (x, y)"""
+	appeared, disappeared, taken are (row, col), not (x, y)"""
+	piece = locations[disappeared[0]][disappeared[1]].piece
+	color = locations[disappeared[0]][disappeared[1]].color
+	locations[disappeared[0]][disappeared[1]].piece = None
+	locations[disappeared[0]][disappeared[1]].color = None
 	if not appeared:
 		# A piece was taken
-		pass
+		locations[taken[0]][taken[1]].piece = piece
+		locations[taken[0]][taken[1]].color = color
 	else:
 		# Generic move
-		piece = locations[disappeared[0]][disappeared[1]].piece
-		color = locations[disappeared[0]][disappeared[1]].color
-		locations[disappeared[0]][disappeared[1]].piece = None
-		locations[disappeared[0]][disappeared[1]].color = None
 		locations[appeared[0]][appeared[1]].piece = piece
 		locations[appeared[0]][appeared[1]].color = color
 		
 
 def location_array_to_board_string(locations):
+	board = [ '________' ,
+			  '________' ,
+			  '________' ,
+			  '________' ,
+			  '________' ,
+			  '________' ,
+			  '________' ,
+			  '________' ]
+	for row in range(len(locations)):
+		for col in range(len(locations[row])):
+			if locations[row][col].piece:
+				board[row][col] = locations[row][col].piece
+	return board
+
+def board_string_to_fen(board, rotation):
 	pass
 
-def location_array_to_fen(locations, rotation):
-	pass
+def _rotate_board_string(board, rotation):
+	rotated = board[:]
+	if rotation == 180:
+		rotated = rotated[::-1]
+		for row in range(len(rotated)):
+			rotated[row] = rotated[row][::-1]
+	elif rotation == 90:
+		for row in range(len(rotated)):
+			for col in range(len(rotated[row])):
+				rotated[len(rotated) - 1 - col][row] = board[row][col]
+	elif rotation == -90:
+		for row in range(len(rotated)):
+			for col in range(len(rotated[row])):
+				rotated[col][len(rotated[row]) - 1 - row] = board[row][col]		
+
