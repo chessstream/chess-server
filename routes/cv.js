7f52ebf96f822c37e8a6c43bc7161a422b3de37c
Smitha Milli <smitha.milli@gmail.com>
2014-04-05 22:55:41 -0700

    Revert 674bac7..4347045
    This rolls back to commit 674bac7c2c1f2485d617a2dd9b22f3c8d06246c6.

--- routes/cv.js
+++ routes/cv.js
@@ -1,24 +1,15 @@
-var fs = require('fs')
-  , path = require('path');
-
 var chessCV = require('./../lib/chess-cv/')
 var chessWeb = require('./../lib/chess-web');
 
-var fileCounter = 0;
-
 exports.analyze = function(req, res){
-  var id = req.body.id;
-  var img = req.files.img;
-  if (!id || !img){
-    res.end('error');
-  }
-  console.log(req.files.img.path);
-  var root = path.dirname(require.main.filename);
-  fs.rename(path.join('/chess-server/', req.files.img.path), path.join('/chess-server/', 'uploads/' + fileCounter + '.jpeg'), function(err){
-    if (err) throw err;
-    console.log('renamed complete');
-  });
-  var fen = chessCV.analyze(id, img);
-  chessWeb.updateGame(id, fen);
-  res.end('success');
+	var id = req.body.id;
+	var img = req.files.img;
+	console.log(id);
+	console.log(req.files);
+	if (!id || !img){
+		res.end('error');
+	}
+	var fen = chessCV.analyze(id, img);
+	chessWeb.updateGame(id, fen);
+	res.end('success');
 }
