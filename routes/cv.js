43daa8545c7142eeb9fef912836acecf6374f575
Smitha Milli <smitha.milli@gmail.com>
2014-04-05 22:37:04 -0700

    temporarily upload to another directory

--- routes/cv.js
+++ routes/cv.js
@@ -1,15 +1,26 @@
+fs = require('fs');
+
 var chessCV = require('./../lib/chess-cv/')
 var chessWeb = require('./../lib/chess-web');
 
+var fileCounter = 0;
+
 exports.analyze = function(req, res){
-	var id = req.body.id;
-	var img = req.files.img;
-	console.log(id);
-	console.log(req.files);
-	if (!id || !img){
-		res.end('error');
-	}
-	var fen = chessCV.analyze(id, img);
-	chessWeb.updateGame(id, fen);
-	res.end('success');
+  var id = req.body.id;
+  var img = req.files.img;
+  if (!id || !img){
+    res.end('error');
+  }
+  fs.readFile(req.files.img.path, function (err, data) {
+    var counter = fileCounter++;
+    var newPath = path.join(__dirname, 'uploads/' + counter + '.jpeg');
+    fs.writeFile(newPath, data, function (err) {
+      res.end('error');
+    });
+  });
+  console.log(id);
+  console.log(req.files);
+  var fen = chessCV.analyze(id, img);
+  chessWeb.updateGame(id, fen);
+  res.end('success');
 }
