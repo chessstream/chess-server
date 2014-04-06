06903fb6e965d2d1bdad52663acc00beff88ea71
Davis Foote <djfoote@berkeley.edu>
2014-04-06 08:23:22 -0700

    finished implementing string utils

--- lib/chess-cv/py-chess-cv/chess_cv/string_utils.py
+++ lib/chess-cv/py-chess-cv/chess_cv/string_utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 from game import Location
+from functools import reduce
 
 def board_string_to_location_array(board):
 	"""A board string is a python list of 8 strings of 8 characters
@@ -50,29 +51,42 @@ def update_location_array(locations, squares, disappeared, appeared, taken):
 		
 
 def location_array_to_board_string(locations):
-	board = [ '________' ,
-			  '________' ,
-			  '________' ,
-			  '________' ,
-			  '________' ,
-			  '________' ,
-			  '________' ,
-			  '________' ]
+	board = [['_' for _ in range(8)] for _ in range(8)]
 	for row in range(len(locations)):
 		for col in range(len(locations[row])):
 			if locations[row][col].piece:
 				board[row][col] = locations[row][col].piece
+	for row in range(len(board)):
+		board[row] = reduce(lambda a, b: a + b, board[row])
 	return board
 
-def board_string_to_fen(board, rotation):
-	pass
+def board_string_to_fen(board, rotation, color_to_move):
+	if rotation:
+		board = _rotate_board_string(board, rotation)
+	fen = ''
+	for row in board:
+		col = 0
+		while col < len(row):
+			if row[col] != '_':
+				fen += row[col]
+				col += 1
+			else:
+				num_empty_squares = 0
+				while col < len(row) and row[col] == '_':
+					col += 1
+					num_empty_squares += 1
+				fen += str(num_empty_squares)
+		fen += '/'
+	fen = fen[:-1]
+	fen += ' ' + color_to_move.lower() + " KQkq - 0 1"
+	return fen
 
 def _rotate_board_string(board, rotation):
 	rotated = board[:]
+	for row in range(len(rotated)):
+		rotated[row] = ['_' for _ in range(len(rotated[row]))]
 	if rotation == 180:
-		rotated = rotated[::-1]
-		for row in range(len(rotated)):
-			rotated[row] = rotated[row][::-1]
+		rotated = _rotate_board_string(_rotate_board_string(board, 90), 90)
 	elif rotation == 90:
 		for row in range(len(rotated)):
 			for col in range(len(rotated[row])):
@@ -80,5 +94,33 @@ def _rotate_board_string(board, rotation):
 	elif rotation == -90:
 		for row in range(len(rotated)):
 			for col in range(len(rotated[row])):
-				rotated[col][len(rotated[row]) - 1 - row] = board[row][col]		
+				rotated[col][len(rotated[row]) - 1 - row] = board[row][col]	
+	for row in range(len(rotated)):
+		rotated[row] = reduce(lambda a, b: a + b, rotated[row])
+	return rotated
 
+if __name__ == '__main__':
+	board0 = [  'rnbqkbnr' ,
+				'pppppppp' ,
+				'________' ,
+				'________' ,
+				'________' ,
+				'________' ,
+				'PPPPPPPP' ,
+				'RNBQKBNR' ]
+	board90 = [ 'RP____pr' ,
+				'NP____pn' ,
+				'BP____pb' ,
+				'QP____pq' ,
+				'KP____pk' ,
+				'BP____pb' ,
+				'NP____pn' ,
+				'RP____pr' ]
+	board180= [ 'RNBKQBNR' ,
+				'PPPPPPPP' ,
+				'________' ,
+				'________' ,
+				'________' ,
+				'________' ,
+				'pppppppp' ,
+				'rnbkqbnr' ]
