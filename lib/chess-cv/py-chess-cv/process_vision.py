31f0d1b68f18cedfb6ad64ea20a469132c242ac4
Smitha Milli <smitha.milli@gmail.com>
2014-04-06 08:33:48 -0700

    rename

--- lib/chess-cv/py-chess-cv/process_vision.py
+++ lib/chess-cv/py-chess-cv/process_vision.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python
 
 import sys
-import chess_cv
+import lib
 
 print 'Number of arguments:', len(sys.argv), 'arguments.'
 print 'Argument List:', str(sys.argv)
 print sys.argv[0]
 
 if len(sys.argv) == 3:
-    chess_cv.start_game(sys.argv[1], sys.argv[2])
+    lib.start_game(sys.argv[1], sys.argv[2])
 
 elif len(sys.argv) == 4:
-    chess_cv.start_game(sys.argv[1], sys.argv[2], sys.argv[3])
+    lib.start_game(sys.argv[1], sys.argv[2], sys.argv[3])
 
 else:
     print 'This command requires 2 or 3 arguments'
